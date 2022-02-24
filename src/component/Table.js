784f0572664d1e40414fdbbb9d9763f748227e27
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-02-24 19:02:57 -0300

    requisito 2

--- src/component/Table.js
+++ src/component/Table.js
@@ -2,52 +2,67 @@ import React, { useContext } from 'react';
 import { PlanetsContext } from '../context/PlanetsContext';
 
 function Table() {
-  const { data } = useContext(PlanetsContext);
+  const { data, handleFilterInputByName, filterByName } = useContext(PlanetsContext);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.length !== 0 && data.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>
-              {planet.films.map((film, i) => (
-                <p key={ i }>
-                  {film}
-                </p>))}
-            </td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+    <>
+      <section>
+        <label htmlFor="filter">
+          Filtre os planetas pelo nome:
+          <input
+            type="text"
+            id="filter"
+            name="filterPlanetName"
+            data-testid="name-filter"
+            value={ filterByName }
+            onChange={ handleFilterInputByName }
+          />
+        </label>
+      </section>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.length !== 0 && data.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>
+                {planet.films.map((film, i) => (
+                  <p key={ i }>
+                    {film}
+                  </p>))}
+              </td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
--- src/context/PlanetsContext.js
+++ src/context/PlanetsContext.js
@@ -5,12 +5,30 @@ import fetchPlanetsAPI from '../apiService/fetchPlanetsAPI';
 export const PlanetsContext = createContext();
 
 function PlanetsProvider({ children }) {
+  const [originalData, setOriginalData] = useState([]);
   const [data, setData] = useState([]);
+  const [filter, setFilter] = useState({ filterByName: '' });
 
   async function fetchPlanets() {
     const planets = await fetchPlanetsAPI();
     const results = planets.results.filter((item) => delete item.residents);
     setData(results);
+    setOriginalData(results);
+  }
+
+  function checkConditionsToFilterByName(value) {
+    if (value.length !== 0) {
+      const dataFilterName = data
+        .filter((planet) => planet.name.toLowerCase().includes(value));
+      setData(dataFilterName);
+    } else {
+      setData(originalData);
+    }
+  }
+
+  function handleFilterInputByName({ target }) {
+    setFilter({ ...filter, filterByName: target.value });
+    checkConditionsToFilterByName(target.value);
   }
 
   useEffect(() => {
@@ -20,6 +38,8 @@ function PlanetsProvider({ children }) {
   const contextValue = {
     data,
     setData,
+    handleFilterInputByName,
+    filterByName: filter.filterByName,
   };
 
   return (
